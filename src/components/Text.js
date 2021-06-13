ffdc9c3beca3e522d2429e6e874e4ed1e5525d13
Dekadente <linaresdev@gmail.com>
2021-06-13 15:48:58 -0500

    Add DystoFactory Upgrade logo

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,5 +1,7 @@
 import '../styles/Text.css';
 import '../styles/Global.css';
+import '../styles/Factory.css';
+import factory from '../images/factory.png';
 import {Container} from '@material-ui/core';
 
 function Text() {
@@ -16,7 +18,11 @@ function Text() {
         <h2>Claim free Punks</h2>
         Apart from the airdrop, <a href="https://opensea.io/collection/dystopianpunks" target="_blank" rel="noopener noreferrer"><b>V1</b></a> and <a href="https://opensea.io/collection/3d-dystopianpunks" target="_blank" rel="noopener noreferrer"><b>3D DystopianPunks</b></a> holders will be able to claim the same amount of <b>Punks</b> that they own completely free only by paying the gas fees, the snapshot will be on <b>June 17 at 07:07</b>.  
         <br/><br/>
-        <h2>DystoFactory Upgrade</h2>
+        <div className="Factory">
+        <header className="Factory-header">
+        <img src={factory} className="Factory-logo" alt="DystoFactory Upgrade"/>
+        </header>
+        </div>
         In addition to the airdrop and free <b>Punks</b> to claim, if you have a <a href="https://opensea.io/collection/dystopianpunks" target="_blank" rel="noopener noreferrer"><b>V1</b></a> and an <a href="https://larvalabs.com/cryptopunks" target="_blank" rel="noopener noreferrer"><b>OG CryptoPunk</b></a> you will be able to access the DystoFactory Upgrade where you can dystopianize your <b>CryptoPunk</b> adapting it to the future with a facial insert and tech, completely free without paying gas, you just have to contact me by <a href="https://discord.gg/RMPkyYzZga" target="_blank" rel="noopener noreferrer"><b>Discord</b></a> or <a href="https://twitter.com/DystopianPunks" target="_blank" rel="noopener noreferrer"><b>Twitter</b></a> to list you.
         <br/><br/>
         <h2>Launch prices</h2>
