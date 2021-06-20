fcedd91995b57619cee11df031790cd676b2c1a4
Dekadente <linaresdev@gmail.com>
2021-06-20 14:31:53 -0500

    Add reveal date

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="DystopianPunks V2" />
         <p style={{lineHeight: "calc(12px + 2vmin)"}}>
-        Welcome to a tech-filled dystopian future <br/> where only <b>2077 Punks</b> survived.
+        Welcome to a tech-filled dystopian future <br/> where only <b>2077 Punks</b> survived. <br/><br/>       
+        Official DystoPunks reveal will take place on <br/> <b>June 24, 17:07 UTC</b>
         </p>
       </header>
     </div>
