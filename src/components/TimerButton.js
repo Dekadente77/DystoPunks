1e5d63a293465ec782d5241561ee106012888ef5
Dekadente <linaresdev@gmail.com>
2021-06-20 16:52:11 -0500

    Add 20 minutes

--- src/components/TimerButton.js
+++ src/components/TimerButton.js
@@ -37,7 +37,7 @@ function TimerButton() {
     let interval = useRef();
 
     const startTimer = () => {
-        const countdownDate = new Date('Jun 20, 2021 17:07:07 GMT-05:00').getTime();
+        const countdownDate = new Date('Jun 20, 2021 17:27:07 GMT-05:00').getTime();
 
         interval = setInterval(() => {
             const now = new Date().getTime();
