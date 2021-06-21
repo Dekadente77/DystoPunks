ccd7faa1264af55f163933bdaf1586e0f7cf962d
Dekadente <linaresdev@gmail.com>
2021-06-20 19:56:18 -0500

    Fix more things

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="DystopianPunks V2" />
         <p style={{lineHeight: "calc(12px + 2vmin)"}}>
         Welcome to a tech-filled dystopian future <br/> where only <b>2077 Punks</b> survived. <br/><br/>       
-        <b b style={{color: '#E54286'}}>Sold out!</b> You can buy on <a href="https://opensea.io/collection/dystopianpunks" target="_blank" rel="noopener noreferrer"><b>OpenSea</b></a>.
+        <b b style={{color: '#E54286'}}>Sold out!</b> You can buy on <a href="https://opensea.io/collection/dystopianpunks-v2" target="_blank" rel="noopener noreferrer"><b>OpenSea</b></a>.
         </p>
       </header>
     </div>
