3fa2863bbc5bc8ccb41ca36ebf7e5344676b2b01
Dekadente <linaresdev@gmail.com>
2021-08-24 20:00:05 -0500

    Tittle fixed

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="DystopianPunks V2" />
+        <img src={logo} className="App-logo" alt="DystoPunks V2" />
         <p style={{lineHeight: "calc(12px + 2vmin)"}}>
         Welcome to a tech-filled dystopian future <br/> where only <b>2077 Punks</b> survived. <br/><br/>       
         <b style={{color: '#E54286'}}>Web under maintenance!</b>
