717196758c5e43c627b3137cbd1c1cd37f92c7fe
Dekadente <linaresdev@gmail.com>
2021-10-15 23:18:36 -0500

    FINAL 2

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,6 @@ function App() {
         <img src={logo} className="App-logo" alt="DystoPunks V2" />
         <p style={{lineHeight: "calc(12px + 2vmin)"}}>
         Welcome to a tech-filled dystopian future <br/> where only <b>2077 Punks</b> survived. <br/><br/>       
-        <b style={{color: '#E54286'}}>Web under maintenance!</b>
         </p>
       </header>
     </div>
